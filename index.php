bc0c076ba53169168ed941b648105397677cebc4
Ritesh Ghimire <38576994+riteshghimire9090@users.noreply.github.com>
2020-04-15 09:37:56 +0545

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       recipe-rating
+ * Text Domain:      rating-for-recipe
  * Domain Path:       /languages
  */
 
